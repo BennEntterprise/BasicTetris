beec9f3c716406eecb5208dff14e12f2c7edafe9
BennEntterprise <kyle@BennEntterprise.com>
2019-06-29 23:02:11 -0400

    express set up

---
+++ app.js
@@ -0,0 +1,16 @@
+//App Set Up
+const express = require('express');
+const app = express();
+const port = process.env.PORT ||  3000;
+
+const morgan = require('morgan');
+
+app.use(express.static('public'));
+app.use(morgan());
+
+app.get('/', function(req, res){
+  res.sendFile('index.html')
+})
+
+
+app.listen(port, console.log(`Listening on Port: ${port}`));
